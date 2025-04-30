e0ae63ae0130cf7a145a188df03aa1dca110053d
myoko <m.yokoyama@centernet.co.jp>
2025-04-30 18:01:52 +0900

    ドキュメント更新

---
+++ publish/Calculator/wwwroot/clipboard.js
@@ -0,0 +1,11 @@
+﻿window.pasteFromClipboard = async () => {
+    try {
+        // Clipboard APIを利用してクリップボードのテキストを取得
+        const text = await navigator.clipboard.readText();
+        console.log("クリップボードから貼り付けた内容:", text);
+        return text;
+    } catch (err) {
+        console.error("貼り付けに失敗しました:", err);
+        return null;
+    }
+};
